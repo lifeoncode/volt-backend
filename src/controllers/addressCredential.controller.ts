cb0fb3bc82d1f3cf79bbc86611d0ad5b495a8992
lifeoncode <nduduzo@yellogarden.co.za>
2025-05-10 20:55:00 +0200

    CRUD operations with Auth

--- src/controllers/addressCredential.controller.ts
+++ src/controllers/addressCredential.controller.ts
@@ -1,7 +1,11 @@
 import {Request, RequestHandler, Response} from 'express';
 import logger from "../middleware/logger";
 import {resolveErrorType} from "../util/helper";
-import {createAddressCredentialService} from "../services/addressCredential.service";
+import {
+    createAddressCredentialService, deleteAddressCredentialService,
+    getAllAddressCredentialsService,
+    getSingleAddressCredentialsService, updateAddressCredentialService
+} from "../services/addressCredential.service";
 import {AddressCredential} from "../util/interface";
 
 export const createAddressCredential: RequestHandler = async (req: Request, res: Response): Promise<void> => {
@@ -12,7 +16,7 @@ export const createAddressCredential: RequestHandler = async (req: Request, res:
 
         const newAddressCredential = await createAddressCredentialService({label, city, street, state, town, user:userId, zip_code});
         res.status(201).json(newAddressCredential);
-        logger.info(`new address credential: ${newAddressCredential.id}`);
+        logger.info(`user: ${userId} created address credential: ${newAddressCredential.id}`);
     } catch (err: unknown) {
         if (err instanceof Error) {
             logger.error(err.message);
@@ -22,16 +26,71 @@ export const createAddressCredential: RequestHandler = async (req: Request, res:
     }
 }
 
-
-export const getAllAddressCredentials = async (req: Request, res: Response) => {
+export const getAllAddressCredentials: RequestHandler = async (req: Request, res: Response):Promise<void> => {
+    try {
+        const userId:number|undefined = req.user?.userId;
+     const addressCredentials = await getAllAddressCredentialsService(userId);
+     res.status(200).json(addressCredentials);
+     logger.info(`user: ${userId} fetched all address credentials`);
+    } catch (err: unknown) {
+        if (err instanceof Error) {
+            logger.error(err.message);
+            const errorType: number = resolveErrorType(err.message);
+            res.status(errorType).json({message: err.message});
+        }
+    }
 }
 
-export const getSingleAddressCredential = async (req: Request, res: Response) => {
+export const getSingleAddressCredential: RequestHandler = async (req: Request, res: Response):Promise<void> => {
+    try {
+        const userId: number | undefined = req.user?.userId;
+        const {id} = req.params;
+        const credential = await getSingleAddressCredentialsService(userId, Number(id));
+        res.status(200).json(credential);
+        logger.info(`user: ${userId} fetched single address credential: ${credential?.id}`);
+    }catch(err: unknown) {
+        if (err instanceof Error) {
+            logger.error(err.message);
+            const errorType: number = resolveErrorType(err.message);
+            res.status(errorType).json({message: err.message});
+        }
+    }
 }
 
-export const updateAddressCredential = async (req: Request, res: Response) => {
+export const updateAddressCredential:RequestHandler = async (req: Request, res: Response):Promise<void> => {
+    try {
+        const userId: number | undefined = req.user?.userId;
+        const {id} = req.params;
+        const {label, city, street, state, zip_code, town} = req.body;
+        if (!label && !city && !street && !state && !zip_code && !town) throw new Error("missing credentials");
+
+        const newCredentialData = {label, city, street, state, zip_code, town};
+        const updatedCredential = await updateAddressCredentialService(userId, Number(id), newCredentialData);
+
+        res.status(200).json(updatedCredential);
+        logger.info(`user: ${userId} updated address credential: ${updatedCredential?.id}`);
+    }catch(err: unknown) {
+        if (err instanceof Error) {
+            logger.error(err.message);
+            const errorType: number = resolveErrorType(err.message);
+            res.status(errorType).json({message: err.message});
+        }
+    }
 }
 
-export const deleteAddressCredential = async (req: Request, res: Response) => {
-}
+export const deleteAddressCredential: RequestHandler = async (req: Request, res: Response):Promise<void> => {
+    try {
+        const userId: number | undefined = req.user?.userId;
+        const {id} = req.params;
+        const deletedCredential = await deleteAddressCredentialService(userId, Number(id));
 
+        res.status(200).json(deletedCredential);
+        logger.info(`user: ${userId} deleted address credential: ${deletedCredential}`);
+    }catch(err: unknown) {
+        if (err instanceof Error) {
+            logger.error(err.message);
+            const errorType: number = resolveErrorType(err.message);
+            res.status(errorType).json({message: err.message});
+        }
+    }
+}
--- src/services/addressCredential.service.ts
+++ src/services/addressCredential.service.ts
@@ -10,3 +10,31 @@ export const createAddressCredentialService = async ({label, city, street, state
 
     return prisma.addressCredential.create({data: {label, city, street, state, town, zip_code, user:{connect:{id:user}}}});
 }
+
+export const getAllAddressCredentialsService = async (userId:number | undefined) => {
+    return prisma.addressCredential.findMany({where: {user_id: userId}});
+}
+
+export const getSingleAddressCredentialsService = async (userId:number | undefined, credentialId: number) => {
+    const credentials = await prisma.addressCredential.findMany({where: {user_id: userId}})
+    if (credentials.length === 0) throw new Error("No credentials found");
+
+    const matchedCredential = credentials.find(item => item.id === credentialId);
+    if (!matchedCredential) throw new Error("No credentials found");
+
+    return matchedCredential;
+}
+
+export const updateAddressCredentialService = async (userId:number | undefined, credentialId: number, newCredentialData:any) => {
+    const matchedCredential = await prisma.addressCredential.findFirst({where: {user_id: userId, id: credentialId}});
+    if (!matchedCredential) throw new Error("No credentials found");
+
+    return prisma.addressCredential.update({where: {user_id: userId, id: credentialId}, data: newCredentialData})
+}
+
+export const deleteAddressCredentialService = async (userId:number | undefined, credentialId: number) => {
+    const matchedCredential = await prisma.addressCredential.findFirst({where: {user_id: userId, id: credentialId}});
+    if (!matchedCredential) throw new Error("No credentials found");
+
+    return prisma.addressCredential.delete({where: {user_id: userId, id: credentialId}})
+}
